ce3e0932278e4bc71c05a1cdcac68f9ba0352412
sccola <sccola2020@gmail.com>
2020-07-22 22:02:36 +0100

    staging talentpool login page

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// function animateNavToggler(el) {
+//   el.classList.toggle('change');
+// }
+
+// document.querySelector('[data-nav-toggler]')
+//   .addEventListener('click', event => {
+//     const navToggler = document.querySelector('.custom-nav-toggler-icon');
+//     navToggler.classList.toggle('open');
+//   }, false);
+
+// document.querySelectorAll('.nav-item')
+//   .forEach((el, key, parent) => {
+//     el.addEventListener('click', (event) => {
+
+//     }, false);
+//   });
